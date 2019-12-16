996a5aca8f5fb3566079c512f1749169c8a435cd
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-12-16 11:15:19 -0800

    finished with num 3

--- hashMapDrills.js
+++ hashMapDrills.js
@@ -15,9 +15,18 @@ function main(){
     lotr.set('LadyOfLight','Galadriel');
     lotr.set('HalfElven','Arwen');
     lotr.set('Ent','Treebeard');
-
-    console.log(lotr);
-    console.log(lotr.get('Hobbit'));
+    //5, 28, 19, 15, 20, 33, 12, 17, 10
+    console.log(5%9);
+    console.log(28%9);
+    console.log(19%9);
+    console.log(15%9);
+    console.log(20%9);
+    console.log(33%9);
+    console.log(12%9);
+    console.log(17%9);
+    console.log(10%9);
+    //console.log(lotr);
+    //console.log(lotr.get('Hobbit'));
 }
 
 main();
@@ -29,4 +38,21 @@ main();
 // Output is going to be 20,10
 
 
-// Question 03
+// Question 03.1
+// This our final hash map after Open addressing corrections. ** means it was relocated due to collision.
+// 22	88*		58	4	15**	28	17**
+// 0	1	2	3	4	5	6	7
+							
+// 59**	31	10					
+// 8	9	10	11	
+
+//Question 3.2
+// 10						
+// 19					33	
+// 28					15	
+// Head	20	12		5	Head	
+// 0	1	2	3	4	5	6	7
+                        
+                        
+// 17							
+// 8	9						
